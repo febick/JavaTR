24db0c7a45784b5622ffbbf0c923be68e4403d6d
febick <febick29@gmail.com>
2021-05-29 08:42:04 +0300

    log2 (func + test)

--- BitewaseOperation/src/OperationTests.java
+++ BitewaseOperation/src/OperationTests.java
@@ -53,5 +53,16 @@ class OperationTests {
 		assertEquals(20, a);
 		assertEquals(10, b);
 	}
+	
+	@Test
+	void log2Test() {
+		assertEquals(1, Operations.log2(3));
+		assertEquals (7, Operations.log2(128));
+		assertEquals(8, Operations.log2(256));
+		assertEquals(8, Operations.log2(400));
+		assertEquals(10, Operations.log2(1024));
+		assertEquals(10, Operations.log2(-1024));
+	}
+
 
 }
--- BitewaseOperation/src/Operations.java
+++ BitewaseOperation/src/Operations.java
@@ -7,13 +7,7 @@ public class Operations {
 	 * @return value of nBit in the given number (see tests)
 	 */
 	public static int getBit(int number, int nBit) {
-		int bitValue = 0;
-		
-		//here your code goes 
-		if (number << ~nBit < 0) {
-			bitValue = 1;
-		}
-		
+		int bitValue = (number << ~nBit < 0) ? 1 : 0;
 		return bitValue;
 	}
 	/**
@@ -25,8 +19,7 @@ public class Operations {
 	 */
 	public static int setBit(int number, int nBit) {
 		int numberRes = number;
-		
-		//here your code goes 
+
 		int newValue = 1 << nBit;
 		numberRes = numberRes | newValue;
 		
@@ -41,13 +34,35 @@ public class Operations {
 	 */
 	public static int resetBit(int number, int nBit) {
 		int numberRes = number;
-		
-		//here your code goes 
+
 		int newValue = 1 << nBit;
 		numberRes = numberRes ^ newValue;
 
 		return numberRes;
 	}
+	
+	/**
+	 * 
+	 * @param n
+	 * @return logN - power of 2 to get n (see tests)
+	 */
+	public static int log2(long n) {
+		// Храним в переменной кол-во итераций
+		var result = 0;
+		
+		// Проверяем не является ли число отрицательным, в этом случае - меняем знак, т.к. нельзя вычитать логорифм у отрицательного числа
+		var tmpValue = n < 0 ? -n : n;
+		
+		// Вычисляем нужное количество итераций для возведения в степень
+		do {
+			tmpValue = tmpValue / 2;
+			result++;	
+		} while (tmpValue > 1);
+		
+		
+		return result;
+	}
+
 
 
 }
