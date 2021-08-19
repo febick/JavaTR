ab8adcbe95a8320a2900aaef5bb5f8506875844b
febick <febick29@gmail.com>
2021-08-19 19:31:30 +0300

    Connections init

--- Connections/src/ConnectionsPoolImpl.java
+++ Connections/src/ConnectionsPoolImpl.java
@@ -1,11 +1,19 @@
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class ConnectionsPoolImpl implements ConnectionsPool {
 
-    private ConnectionsList connectionsList = new ConnectionsList(10);
+    private static int limit;
+    private static int size;
+    private static HashMap<Integer, Node> map;
+    private final ConnectionsList connectionsList;
+
+    public ConnectionsPoolImpl(int limit) {
+        this.limit = limit;
+        this.map = new HashMap<>();
+        this.connectionsList = new ConnectionsList();
+    }
 
     private static class Node {
         public Connection connection;
@@ -17,32 +25,17 @@ public class ConnectionsPoolImpl implements ConnectionsPool {
     }
 
     private static class ConnectionsList {
-        HashMap<Integer, Node> map = new HashMap<>();
         Node head;
         Node tail;
-        int size;
-        int limit;
-        ConnectionsList(int limit) {
-            this.limit = limit;
-        }
 
         public void add(Connection connection) {
-            if (size > limit) { removeTail(); }
-            Node current = map.get(connection.getId());
-            if (current != null) {
-                moveToTop(current);
-            } else {
-                Node newNode = new Node(connection);
-                newNode.next = head;
-                head.prev = newNode;
-                head = newNode;
-                size++;
-            }
-         }
-
-         public Connection get(int id) {
-            return map.get(id).connection;
-         }
+            Node newNode = new Node(connection);
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+            map.put(connection.getId(), newNode);
+            size++;
+        }
 
         public void removeTail() {
             map.remove(tail.connection.getId());
@@ -52,30 +45,53 @@ public class ConnectionsPoolImpl implements ConnectionsPool {
         }
 
         private void moveToTop(Node node) {
-            node.prev.next = node.next;
-            node.next.prev = node.prev;
-            node.prev = null;
-            node.next = head;
-            head.prev = node;
-            head = node;
+            if (head != node) {
+                node.prev.next = node.next;
+                node.next.prev = node.prev;
+                node.prev = null;
+                node.next = head;
+                head.prev = node;
+                head = node;
+            }
         }
 
     }
 
     @Override
     public void add(Connection connection) {
-        connectionsList.add(connection);
+        if (size > limit) { connectionsList.removeTail(); }
+
+        Node current = map.get(connection.getId());
+        if (current != null) {
+            connectionsList.moveToTop(current);
+        } else {
+            connectionsList.add(connection);
+        }
     }
 
     @Override
-    public Connection get(int id) throws Exception {
-        Connection res = connectionsList.get(id);
+    public Connection get(int id) throws NoSuchElementException {
+        Connection res = map.get(id).connection;
         if (res == null) { throw new NoSuchElementException(); }
         return res;
     }
 
     @Override
     public Iterator<Connection> iterator() {
-        return null;
+        return new Iterator<Connection>() {
+            Node current = connectionsList.head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public Connection next() {
+                Node res = current;
+                current = current.next;
+                return res.connection;
+            }
+        };
     }
 }
